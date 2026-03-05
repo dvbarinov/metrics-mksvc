36c400c802950e8deec18b389a0c1aed6d1a3730
dvbarinov <d.v.barinov@gmail.com>
2026-03-05 20:22:06 +0300

    Move alertmanager run into sh

--- monitoring/entrypoint.sh
+++ monitoring/entrypoint.sh
@@ -1,5 +1,26 @@
+#!/bin/sh
+set -eu
+
+TEMPLATE=/etc/alertmanager/alertmanager.yml.tmpl
+CONFIG=/etc/alertmanager/alertmanager.yml
+
+echo "Generating Alertmanager config from template..."
+
+if [ ! -f "$TEMPLATE" ]; then
+  echo "Template not found: $TEMPLATE"
+  exit 1
+fi
+
+# Подставляем только переменные, которые реально используются
+VARS=$(env | cut -d= -f1 | sed 's/^/${/;s/$/}/')
+
 # Генерируем конфиг из шаблона
-envsubst < /etc/alertmanager/alertmanager.yml.tmpl > /etc/alertmanager/alertmanager.yml
+envsubst "$VARS" < "$TEMPLATE" > "$CONFIG"
+
+echo "Config generated:"
+echo "--------------------------------"
+cat "$CONFIG"
+echo "--------------------------------"
 
 # Запускаем Alertmanager
-exec /bin/alertmanager --config.file=/etc/alertmanager/alertmanager.yml "$@"
+exec /bin/alertmanager --config.file="$CONFIG" "$@"
